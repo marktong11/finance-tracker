4a0f4474832bba7555f2f12f2e6f9df21d18fd8c
Mark Tong <marktong11@gmail.com>
2018-02-26 19:39:03 -0500

    commit updates

---
+++ app/controllers/stocks_controller.rb
@@ -0,0 +1,16 @@
+class StocksController < ApplicationController
+
+	def search
+		if params[:stock].blank?
+			flash.now[:danger] = "You have entered an empty search string"
+		else
+			@stock = Stock.new_from_lookup(params[:stock])
+			flash.now[:danger] = "You have entered an incorrect symbol" unless @stock
+		end
+	
+		respond_to do |format|
+			format.js { render partial: 'users/result' }
+		end
+	end
+
+end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,22 @@
 class Stock < ApplicationRecord
+	has_many :user_stocks
+	has_many :users, through: :user_stocks
+
+	def self.find_by_ticker(ticker_symbol)
+		where(ticker: ticker_symbol).first
+	end
+
+	def self.new_from_lookup(ticker_symbol)
+		begin
+			looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
+			price = strip_commas(looked_up_stock.l)
+			new(name: looked_up_stock.name, ticker: looked_up_stock.symbol, last_price: price)
+		rescue Exception => e
+			return nil
+		end
+	end
+
+	def self.strip_commas(number)
+		number.gsub(",", "")
+	end
 end
